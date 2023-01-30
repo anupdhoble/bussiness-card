26e10cb184d61fdc0e9b9db080545f11e3c68416
anupdhoble <anupdhoble15@gmail.com>
2023-01-30 16:43:03 +0530

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,7 @@
 import React from "react"
 export default function Profile(){
-    return(<>
+    return(
         <img className="profile_img" src={process.env.PUBLIC_URL + '/img/profile.jpg'}/>
-            
-        <img className="profile_img"src="https://extension.umaine.edu/diagnostic-lab/wp-content/uploads/sites/22/2018/05/plant-sample-317x317.jpg"></img></>
+   
     )
 }
