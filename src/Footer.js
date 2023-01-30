d276c3d400cd4dddfd38e5cb717ba85945cca18b
Anup Dhoble <83175840+anupdhoble@users.noreply.github.com>
2023-01-30 18:05:40 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
                 <a href="https://twitter.com/dhoble_anup" target="_blank"><img src={process.env.PUBLIC_URL + '/icons/Twitter.png'} /></a>
                 <a href="https://www.facebook.com/anup.dhoble.96" target="_blank"><img src={process.env.PUBLIC_URL + '/icons/Facebook.png'} /></a>
                 <a href="https://www.instagram.com/anupdhoble15/" target="_blank"><img src={process.env.PUBLIC_URL + '/icons/Instagram.png'} /></a>
-                <a href="https://github.com/anupdhoble" target="_blank"><img src={process.env.PUBLIC_URL + '/icons/Github.png'} /></a>
+                <a href="https://github.com/anupdhoble" target="_blank"><img src={process.env.PUBLIC_URL + '/icons/GitHub.png'} /></a>
             </div>
         </footer>
     )
